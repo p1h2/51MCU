7103fa6132ad308412f04d34133c8b856065731e
森狼藤野 <125631246+p1h2@users.noreply.github.com>
2024-11-04 18:35:11 +0800

    Delete oneWire.h

--- oneWire.h
+++
@@ -1,9 +0,0 @@
-#ifndef  _ONEWIRE_H_
-#define  _ONEWIRE_H_
-unsigned char oneWire();
-void DELAY5us(void);
-void send(unsigned char byte);
-unsigned char recive();
-void sendbyte(unsigned char byte);
-unsigned char recivebyte();
-#endif
